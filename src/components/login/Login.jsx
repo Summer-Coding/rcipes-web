eadc9b72355071815338cdf85523b4287b4d9d48
Adam Gardner <adga2012@gmail.com>
2022-05-18 18:58:06 -0600

    chore: make login form cleaner

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,6 @@
 import { Form, Formik } from 'formik';
 import React from 'react';
-import { Button, Card, CardBody, CardHeader } from 'reactstrap';
+import { Button, Card, CardBody, CardHeader, Container } from 'reactstrap';
 import * as yup from 'yup';
 import FormField from '../Form/Field';
 import './login.css';
@@ -13,22 +13,26 @@ const schema = yup.object().shape({
 const Login = () => {
   return (
     <Card>
-      <CardHeader>Login</CardHeader>
+      <CardHeader tag="h3">
+        <span className="ms-3">Login</span>
+      </CardHeader>
       <CardBody>
-        <Formik
-          initialValues={{ email: '', password: '' }}
-          validationSchema={schema}
-          // eslint-disable-next-line no-console
-          onSubmit={(values) => console.log(values)}
-        >
-          <Form>
-            <FormField name="email" required />
-            <FormField name="password" type="password" />
-            <Button type="submit" color="success">
-              Submit
-            </Button>
-          </Form>
-        </Formik>
+        <Container>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validationSchema={schema}
+            // eslint-disable-next-line no-console
+            onSubmit={(values) => console.log(values)}
+          >
+            <Form>
+              <FormField name="email" required />
+              <FormField name="password" type="password" />
+              <Button type="submit" color="success">
+                Submit
+              </Button>
+            </Form>
+          </Formik>
+        </Container>
       </CardBody>
     </Card>
   );
