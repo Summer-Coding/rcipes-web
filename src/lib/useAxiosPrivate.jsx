530d6e7d7984bf197cb09b15c888ec90af7c5f78
Adam Gardner <adga2012@gmail.com>
2022-07-02 00:38:26 -0600

    fix: check for null session
    this can happen when being transferred from an outside website.

--- src/lib/useAxiosPrivate.jsx
+++ src/lib/useAxiosPrivate.jsx
@@ -2,15 +2,22 @@ import { useEffect } from 'react';
 import { supabase } from './supabaseClient.ts';
 import { axiosPrivate } from '../api/axiosPrivate.ts';
 
+const absentTokenError = 'No access token present. Please try again.';
+
 export const useAxiosPrivate = () => {
   useEffect(() => {
-    const session = supabase.auth.session();
-
     const requestIntercept = axiosPrivate.interceptors.request.use(
       (config) => {
+        const session = supabase.auth.session();
+
+        if ((session?.access_token ?? '').length === 0) {
+          return Promise.reject(absentTokenError);
+        }
+
         if (!config.headers['Authorization']) {
           config.headers['Authorization'] = `Bearer ${session.access_token}`;
         }
+
         return config;
       },
       (error) => Promise.reject(error),
@@ -30,8 +37,8 @@ export const useAxiosPrivate = () => {
             return Promise.reject(err);
           }
 
-          if (!session?.access_token) {
-            return Promise.reject();
+          if ((session?.access_token ?? '').length === 0) {
+            return Promise.reject(absentTokenError);
           }
 
           prevRequest.headers[
